72af7f02aa6c97258f04b0ef03cf60d78326288e
kittykatt210 <keperry30@icloud.com>
2023-10-29 16:23:34 -0500

    Added function to generate the SVG.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const shapes = require('./lib/shapes');
+const generateSVG = require('./lib/generateSVG');
 
 // Array of shapes
 const shapes = ['Circle', 'Square', 'Triangle'];
@@ -9,7 +9,7 @@ const questions = [
     { type: 'input', message: 'What would you like to display? Enter up to three characters:', name: 'text', validate: (value) => {if(value) {return true} else { return 'Please enter three characters to display.'}}},
     { type: 'input', message: 'What text color do you want? Enter a hexidecimal number or color name:', name: 'textColor', validate: (value) => {if(value) {return true} else { return 'Please enter a text color.'}}},
     { type: 'list', message: 'Select a shape for your logo:', choices: shapes, name: 'shape'},
-    { type: 'input', message: 'What shape color do you want? Enter a hexidecimal number or color name:', name: 'textColor', validate: (value) => {if(value) {return true} else { return 'Please enter a shape color.'}}}
+    { type: 'input', message: 'What shape color do you want? Enter a hexidecimal number or color name:', name: 'shapeColor', validate: (value) => {if(value) {return true} else { return 'Please enter a shape color.'}}}
 ];
 
 // Function to write .svg file
