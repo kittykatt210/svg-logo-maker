30608367c90169b837a797fd76b0691e509217e2
kittykatt210 <keperry30@icloud.com>
2023-10-29 20:09:47 -0500

    Finished shapes.test.js and added maxlength to input prompt for logo text.

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 // Packages needed for this application
 const inquirer = require('inquirer');
+const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
 const fs = require('fs');
 const generateSVG = require('./lib/generateSVG');
+
+// Registering the prompt with inquirer
+inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
 
 // Array of shapes for list used in the questions array
 const shapes = ['Circle', 'Square', 'Triangle'];
 
 // Array of questions for user input
 const questions = [
-    { type: 'input', message: 'What would you like to display? Enter up to three characters:', name: 'text', validate: (value) => {if(value) {return true} else { return 'Please enter three characters to display.'}}},
-    { type: 'input', message: 'What text color do you want? Enter a hexidecimal number or color name:', name: 'textColor', validate: (value) => {if(value) {return true} else { return 'Please enter a text color.'}}},
+    { type: 'maxlength-input', message: 'Enter three characters for your logo:', name: 'text', maxLength: 3},
+    { type: 'input', message: 'Enter text color (color name or hexadecimal number):', name: 'textColor', validate: (value) => {if(value) {return true} else { return 'Please enter a text color.'}}},
     { type: 'list', message: 'Select a shape for your logo:', choices: shapes, name: 'shape'},
-    { type: 'input', message: 'What shape color do you want? Enter a hexidecimal number or color name:', name: 'shapeColor', validate: (value) => {if(value) {return true} else { return 'Please enter a shape color.'}}}
+    { type: 'input', message: 'Enter shape color (color name or hexadecimal number):', name: 'shapeColor', validate: (value) => {if(value) {return true} else { return 'Please enter a shape color.'}}}
 ];
 
 // Function to write .svg file
 function writeToFile(data) {
-    const filename = `${data.shape}.svg`;
+    const filename = `logo.svg`;
     fs.writeFile(`./examples/${filename}`, generateSVG(data), (err) => err ? console.log(err) : console.log('Generated logo.svg'))
 }
 
